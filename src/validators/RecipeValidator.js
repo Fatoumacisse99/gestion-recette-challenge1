02c277d413061a66469263cc4757c19a505a277a
Fatoumacisse99 <cfama673@gmail.com>
2024-10-02 02:08:11 +0200

    Améliorer les règles de validation dans RecipeValidator

---
+++ src/validators/RecipeValidator.js
@@ -0,0 +1,90 @@
+import { check, param, validationResult } from 'express-validator';
+
+
+// Validation pour la création d'une recette
+const validateCreateRecipe = () => {
+  return [
+    check('titre')
+      .not()
+      .isEmpty()
+      .withMessage('Le titre ne peut pas être vide!')
+      .bail()
+      .isLength({ min: 6, max: 100 })
+      .withMessage('Le titre doit contenir entre 6 et 100 caractères!')
+      .bail(),
+      
+    check('ingredients')
+      .not()
+      .isEmpty()
+      .withMessage('Les ingrédients ne peuvent pas être vides!')
+      .bail()
+      .isLength({ min: 10, max: 500 })
+      .withMessage('Les ingrédients doivent contenir entre 10 et 500 caractères!'),
+      
+    check('type')
+      .not()
+      .isEmpty()
+      .withMessage('Le type de recette est requis!')
+      .bail()
+      .isIn(['entrée', 'plat principal', 'dessert'])
+      .withMessage('Le type de recette doit être \'entrée\', \'plat principal\' ou \'dessert\'!')
+  ];
+};
+
+// Validation pour la mise à jour d'une recette
+const validateUpdateRecipe = () => {
+  return [
+    param('id')
+      .not()
+      .isEmpty()
+      .withMessage('L\'ID est requis pour mettre à jour une recette!')
+      .bail()
+      .isInt({ gt: 0 })
+      .withMessage('L\'ID doit être un entier positif!'),
+    
+    check('titre')
+      .optional()
+      .isLength({ min: 6, max: 100 })
+      .withMessage('Le titre doit contenir entre 6 et 100 caractères!')
+      .bail(),
+    check('ingredients')
+      .optional()
+      .isLength({ min: 10, max: 500 })
+      .withMessage('Les ingrédients doivent contenir entre 10 et 500 caractères!'),
+    
+    check('type')
+      .optional()
+      .isIn(['Entree', 'Plat principal', 'Dessert'])
+      .withMessage('Le type de recette doit être \'Entree\', \' Plat principal\' ou \'dessert\'!')
+  ];
+};
+
+// Validation pour la suppression d'une recette
+const validateDeleteRecipe = () => {
+  return [
+    param('id')
+      .not()
+      .isEmpty()
+      .withMessage('L\'ID est requis pour supprimer une recette!')
+      .bail()
+      .isInt({ gt: 0 })
+      .withMessage('L\'ID doit être un entier positif.')
+  ];
+};
+
+// Middleware de validation
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  next();
+};
+
+// Exportation des fonctions
+export {
+  validateCreateRecipe,
+  validateUpdateRecipe,
+  validateDeleteRecipe,
+  validate,
+};
